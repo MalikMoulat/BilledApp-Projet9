d588f4a94ab99ada5b13ae8c3cab13b07d8a0ccb
MalikMoulat <malik.moulat@hotmail.fr>
2023-02-19 00:40:42 +0100

    fix admin dashboard bug

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -88,7 +88,10 @@ export default class {
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+    // if (this.counter % 2 === 0) {
+
+    // console.log('Dans le if :', this.counter)
+
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
@@ -96,15 +99,18 @@ export default class {
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
       this.counter ++
-    } else {
-      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+    // } else {
 
-      $('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      `)
-      $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
-    }
+    // console.log('Dans le else :', this.counter)
+    
+    //   $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+
+    //   $('.dashboard-right-container div').html(`
+    //     <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+    //   `)
+    //   $('.vertical-navbar').css({ height: '120vh' })
+    //   this.counter ++
+    // }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
